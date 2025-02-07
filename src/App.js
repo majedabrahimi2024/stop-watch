0422fa4f74aa38eb3e28b31c40898dde4106a882
majed <majedabrahimi2024@gmail.com>
2025-02-07 10:09:47 +0330

    App&styles modified & Hello renamed to Header

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import React , {useState,useEffect} from "react";
 import ReactDOM from "react-dom";
-import Hello from "./Hello.js";
+import Header from "./Header.js";
 import Timer from "./Timer.js";
 import './styles.css';
 import TimeList from "./TimeList.jsx";
 import { TestContext } from "./TestContext.jsx";
 
 const App = ()=>{
-  const [title,setTitle] = useState("hello world !");
+  const [title,setTitle] = useState("Stop Watch");
   const [isLight,setIsLight] = useState(false);
   const [timeArr,setTimeArr] = useState([]);
 
@@ -24,12 +24,12 @@ const App = ()=>{
 
   return (
     <TestContext.Provider value={{
-      timeArr:timeArr,
-      setTimeArr:setTimeArr,
+      timeArr,
+      setTimeArr,
       }}>
       <div className="main" style={{backgroundColor : isLight ? "white": "gray"}}>
       <div className="main-container">
-        <Hello title={title}/>
+        <Header title={title}/>
         <Timer
           isLight={isLight}
           handleSetIsLight={handleSetIsLight}
